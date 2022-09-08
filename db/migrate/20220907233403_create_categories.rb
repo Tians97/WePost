4d3a59cc7228d2efe81539d017fcfb2e36332143
Tianshu Xiao <tx2@g.ucla.edu>
2022-09-08 09:22:02 -0700

    table

---
+++ db/migrate/20220907233403_create_categories.rb
@@ -0,0 +1,9 @@
+class CreateCategories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :categories do |t|
+      t.string :title, null:false
+      t.timestamps
+    end
+    add_index :categories, :title, unique:true
+  end
+end
